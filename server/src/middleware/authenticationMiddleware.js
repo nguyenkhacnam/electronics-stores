5dac342c07df46787fc660136d0328fd4ccb76d3
nguyenkhacnam <nguyenkhacnam06052001@gmail.com>
2023-09-04 00:32:57 +0700

    (backend): Add JWT authentication middleware

---
+++ server/src/middleware/authenticationMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+dotenv.config()
+
+class AuthenticationMiddleware {
+  authMiddleware(req, res, next) {
+    console.log('first', req.headers.token)
+    const token = req.headers.token.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, user) {
+      if (err) {
+        return res.status(404).json({
+          status: 'ERR',
+          message: 'Authentication failed. Invalid or expired token'
+        })
+      }
+      const { payload } = user
+      if (payload.isAdmin) {
+        next()
+      } else {
+        return res.status(404).json({
+          status: 'ERR',
+          message: 'Authentication failed. Invalid or expired token'
+        })
+      }
+    })
+  }
+}
+
+module.exports = new AuthenticationMiddleware
--- server/src/routes/UsersRouter.js
+++ server/src/routes/UsersRouter.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const usersControllers = require('../controllers/UsersControllers')
+const authenticationMiddleware = require('../middleware/authenticationMiddleware')
 
 router.post('/register', usersControllers.createUser)
 router.post('/login', usersControllers.loginUser)
 router.put('/update-user/:id', usersControllers.updateUser)
-router.delete('/delete-user/:id', usersControllers.deleteUser)
+router.delete('/delete-user/:id', authenticationMiddleware.authMiddleware, usersControllers.deleteUser)
 
 module.exports = router
