257413863a525dd94e853b91f0e0d3981bfdae71
nguyenkhacnam <nguyenkhacnam06052001@gmail.com>
2023-09-03 14:18:56 +0700

    (backend): setups project

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config()
+
 const app = express()
-const port = 3001
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
